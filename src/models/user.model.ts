d70d7c3a76b63dd1363703e6b6f8266f38080205
Balakannan <balakannan2522@gmail.com>
2025-03-27 20:00:09 +0530

    fix: mongoose model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -52,7 +52,7 @@ const userSchema = new Schema(
     password: {
       type: String,
     },
-    ...creatorBaseSchema,
+    ...creatorBaseSchema.obj,
   },
   { timestamps: true, collection: 'Users' },
 );

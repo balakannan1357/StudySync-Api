209bce03c7a9320200040b1990d95cef737c85f8
Balakannan <balakannan2522@gmail.com>
2025-03-27 20:01:13 +0530

    feat: routes added

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import { AuthRoute } from '@routes/auth.route';
 import IndexRoute from '@routes/index.route';
 import { UserRoute } from '@routes/users.route';
 import { ValidateEnv } from '@utils/validateEnv';
+import { SubTopicRoute } from './routes/subTopic.route';
+import { WeekPlanRoute } from './routes/weekPlan.route';
 
 ValidateEnv();
 
@@ -11,6 +13,8 @@ const Routes = [];
 Routes.push(new IndexRoute());
 Routes.push(new AuthRoute());
 Routes.push(new UserRoute());
+Routes.push(new SubTopicRoute());
+Routes.push(new WeekPlanRoute());
 
 const app = new App(Routes);
 
